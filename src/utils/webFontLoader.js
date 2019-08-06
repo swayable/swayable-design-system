fc3a634d195b11debf56e9b8d06d84f910b071a5
Josh Dean <jdbdean@gmail.com>
2019-08-06 10:53:01 -0700

    refactor(fonts): use webFontLoader for typekit fonts (#11)

--- src/utils/webFontLoader.js
+++ src/utils/webFontLoader.js
@@ -1,11 +1,12 @@
 /**
  * Web Font Loader takes care of Vue Design System’s font loading.
  * For full documentation, see: https://github.com/typekit/webfontloader
+ *
+ * Source: https://use.typekit.net/dtu7wgq.css
+ * Manage at fonts.adobe.com, account clientsuccess@swayable.com
  */
 import WebFont from 'webfontloader'
 
 WebFont.load({
-  google: {
-    families: ['Fira+Sans:300,400,400i,600,700'],
-  },
+  typekit: { id: 'dtu7wgq' },
 })
