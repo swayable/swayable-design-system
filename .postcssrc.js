31f53ae29be30a737fd4b47ce3f7d0338c61d713
Josh Dean <jdbdean@gmail.com>
2019-11-26 16:17:24 -0800

    install package postcss-color-mod-function

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,23 +3,9 @@
 const plugins = [
   require("postcss-import"),
   require("postcss-url"),
-  // to edit target browsers: use "browserslist" field in package.json
   require("tailwindcss"),
   require("autoprefixer"),
+  require('postcss-color-mod-function'),
 ]
-
-// Temporarily disabled purge css as production was missing used styles
-// if (process.env.NODE_ENV === "production") {
-//   const purgecss = require("@fullhuman/postcss-purgecss")({
-//     // Specify the paths to all of the template files in your project
-//     content: ["./src/**/*.vue"],
-
-//     // Include any special characters you're using in this regular expression
-//     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-//   })
-
-//   // purgecss must be the last plugin
-//   plugins.push(purgecss)
-// }
 
 module.exports = { plugins }
