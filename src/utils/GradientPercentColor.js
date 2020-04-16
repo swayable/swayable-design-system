276c3a23f57dc54e8bdef5602a100cd9b47ba252
Josh Dean <jdbdean@gmail.com>
2020-04-16 11:40:17 -0700

    fix gradient percent color

--- src/utils/GradientPercentColor.js
+++ src/utils/GradientPercentColor.js
@@ -2,29 +2,23 @@ import tokens from './tokens'
 
 const LEFT_RGB = tokens.getColorRGB('pink')
 const RIGHT_RGB = tokens.getColorRGB('azure')
-
+const GRADIENT_MIN = 0.3
+const GRADIENT_MAX = 0.7
 /**
  * Finds the color at a given point in a gradient of pink-blue
  * Used to draw segments of a gradient
- * `gradientMin` and `gradientMax` allow ~mid segments to be more dramatic
- *
- * e.g. a gradient drawn with args
- * `GradientPercentColor(.45, 0, 1)` to `GradientPercentColor(55, 0, 1)`
- *  will be less visible than with args
- * `GradientPercentColor(.45, .2, .8)` to `GradientPercentColor(55, .2, .8)`
+ * `GRADIENT_MIN` and `GRADIENT_MAX` pull the edges of the gradient in,
+ * allowing for a more dramatic change around the center
 */
 
 class GradientPercentColor {
-  constructor(percent, gradientMin=0.2, gradientMax=0.8) {
+  constructor(percent) {
     this.percent = percent
-    this.gradientMin = gradientMin
-    this.gradientMax = gradientMax
   }
 
   get weight() {
-    const { percent, gradientMin, gradientMax } = this
-    const range = gradientMax - gradientMin
-    const weightedRatio = Math.max(percent - gradientMin, 0)
+    const weightedRatio = Math.max(this.percent - GRADIENT_MIN, 0)
+    const range = GRADIENT_MAX - GRADIENT_MIN
     return Math.min(weightedRatio / range, 1)
   }
 
@@ -48,10 +42,15 @@ class GradientPercentColor {
     return Math.round(right + left)
   }
 
-  static drawGradient(gradientStart, gradientStop) {
-    const startColor = new GradientPercentColor(gradientStart).cssValue
-    const endColor = new GradientPercentColor(gradientStop).cssValue
-    return `linear-gradient(to right, ${startColor}, ${endColor})`
+  static drawGradient(pt1, pt2) {
+    const startColor = new GradientPercentColor(pt1).cssValue
+    const endColor = new GradientPercentColor(pt2).cssValue
+
+    
+    const startPct = Math.round(Math.max(0, GRADIENT_MIN - pt1) * 100)
+    const endPct = Math.round(Math.min(1, 1 + GRADIENT_MAX - pt2) * 100)
+
+    return `linear-gradient(to right, ${startColor} ${startPct}%, ${endColor} ${endPct}%)`
   }
 }
 
