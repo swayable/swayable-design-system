1cebff60c601bd32b942a34efe36810310c9c168
Josh Dean <jdbdean@gmail.com>
2020-04-30 15:34:16 -0700

    fix: mobile dropdown width for small buttons

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -11,7 +11,7 @@
       <slot />
     </span>
     <div
-      class='w-full min-w-full md:w-auto md:max-w-screen flex-col z-50'
+      class='min-w-full md:w-auto md:max-w-screen flex-col z-50'
       :class='align ? `absolute ${align}-0` : "relative"'
     >
       <transition
